14e026baec32307167e729be9c3cc94f17e4ba3b
Alexander I.Grafov <alexander.grafov@lazada.com>
2016-01-23 12:27:44 +0300

    Add exit by F10

--- keys.go
+++ keys.go
@@ -6,5 +6,8 @@ func initKeys(g *gocui.Gui) error {
 	if err := g.SetKeybinding("", gocui.KeyCtrlQ, gocui.ModNone, quit); err != nil {
 		return err
 	}
+	if err := g.SetKeybinding("", gocui.KeyF10, gocui.ModNone, quit); err != nil {
+		return err
+	}
 	return nil
 }
